07c11874f4cbc6140a9e32cf9692698fd335dca8
AliceWaddicor <alice.waddicor@endjin.com>
2014-08-10 19:58:18 +0100

    Add CompanyBrand relationship class

--- CandidateParsingAgilityPack/Program.cs
+++ CandidateParsingAgilityPack/Program.cs
@@ -6,7 +6,6 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
-    using System.Security.Cryptography.X509Certificates;
 
     using HtmlAgilityPack;
 
@@ -14,7 +13,47 @@
 
     internal class Program
     {
-        private static List<Candidate> GetCandidatesFromPages(IEnumerable<string> pages, List<string> knownCompanies)
+        private static void Main(string[] args)
+        {
+            // When scraping:
+
+            //var doc = new HtmlDocument();
+            //using (var client = new WebClient())
+            //{
+            //    var filename = Path.GetTempFileName();
+            //    client.DownloadFile("http://en.wikipedia.org/wiki/Nestl%C3%A9", filename);
+            //    doc.Load(filename);
+            // Move rest of the method in here
+            //}
+
+            // When using local files: 
+
+            var knownCompanyBrandRelationships = GetKnownCompanyBrandRelationships();
+
+            var pages = GetPages("C:/Users/Alice/Desktop/TestDocuments");
+
+            var candidates = GetCandidatesFromPages(pages, knownCompanyBrandRelationships);
+
+            var file = new StreamWriter("C:/Users/Alice/Desktop/Candidates.txt");
+
+            foreach (var candidate in candidates)
+            {
+                file.WriteLine(
+                               "Page title: " + candidate.PageTitle + "\r Known company: " + candidate.KnownCompany
+                               + "\r Html & Text: " + candidate.CandidateHtmlAndText + "\r ");
+                Console.WriteLine(
+                                  candidate.PageTitle + ' ' + candidate.KnownCompany + ' '
+                                  + candidate.CandidateHtmlAndText);
+            }
+
+            file.Close();
+
+            Console.ReadLine();
+        }
+
+        private static List<Candidate> GetCandidatesFromPages(
+                IEnumerable<string> pages,
+                List<CompanyBrandRelationship> knownCompanyBrandRelationships)
         {
             var doc = new HtmlDocument();
 
@@ -38,27 +77,29 @@
             //      Find candidates which contain the brand and add to the list of candidates, noting the company + brand for that candidate
             //      For lists & tables, include the previous paragraph if there is one. For paragraphs, check the 3 sibling paragraphs above & below for a mention of the company, include if one is found.
 
+            // 
+
             foreach (var page in pages)
             {
                 doc.Load(page);
 
                 var root = doc.DocumentNode;
 
-                bool containsKnownCompany = false;
+                bool containsKnownRelationship = false;
 
-                List<string> knownCompaniesPresent = new List<string>();
+                var knownOwnerCompaniesPresent = new List<string>();
 
                 // List<HtmlNode> nodesContainingKnownCompany = new List<HtmlNode>();
 
                 // Check if the page contains any known seed company names
-                foreach (var knownCompany in knownCompanies)
+                foreach (var knownRelationship in knownCompanyBrandRelationships)
                 {
-                    string company = knownCompany;
+                    var companyName = knownRelationship.OwnerNames.First();
 
-                    if (root.OuterHtml.Contains(knownCompany))
+                    if (root.OuterHtml.Contains(knownRelationship.OwnerNames.First()))
                     {
-                        containsKnownCompany = true;
-                        knownCompaniesPresent.Add(knownCompany);
+                        containsKnownRelationship = true;
+                        knownOwnerCompaniesPresent.Add(knownRelationship.OwnerNames.First());
                     }
 
                     //var nodes = root.Descendants().Where(d => d.InnerText.Contains(company));
@@ -70,7 +111,7 @@
                 }
 
                 // Skip document if it doesn't contain a seed company name
-                if (!containsKnownCompany)
+                if (!containsKnownRelationship)
                 {
                     continue;
                 }
@@ -86,17 +127,22 @@
                 var tables = root.Descendants("table").ToList();
 
                 var lists = root.Descendants("ul").ToList();
-                  
+
                 var paragraphs = root.Descendants("p").ToList();
 
                 foreach (var table in tables)
                 {
                     // Need a new candidate for each known company, even for the same page segment.
-                    foreach (var knownCompany in knownCompaniesPresent)
+                    foreach (var knownCompany in knownOwnerCompaniesPresent)
                     {
                         if (table.OuterHtml.Contains(knownCompany))
                         {
-                            var candidate = new Candidate { IsTableSegment = true, CandidateHtmlAndText = table.OuterHtml, KnownCompany = knownCompany};
+                            var candidate = new Candidate
+                                                {
+                                                        IsTableSegment = true,
+                                                        CandidateHtmlAndText = table.OuterHtml,
+                                                        KnownCompany = knownCompany
+                                                };
 
                             candidate.Uri = page;
 
@@ -137,14 +183,92 @@
 
                     //Console.WriteLine("Table text: {0}", list.InnerText.Trim());
                 }
-
             }
             return candidates;
         }
 
-        private static List<string> GetKnownCompanies()
+        private static List<CompanyBrandRelationship> GetKnownCompanyBrandRelationships()
         {
-            return new List<string> { "Nestlé", "Cadbury", "Bayer" };
+            var relationships = new List<CompanyBrandRelationship>();
+            relationships.Add(
+                              new CompanyBrandRelationship
+                                  {
+                                          OwnerNames = new List<string>() { "Nestle, Nestlé" },
+                                          BrandNames =
+                                                  new List<string>()
+                                                      {
+                                                              "Buxton",
+                                                              //"Kit Kat",
+                                                              //"Nescafé",
+                                                              //"Smarties",
+                                                              //"Nesquik",
+                                                              //"Stouffer's",
+                                                              //"Maggi",
+                                                              //"L'Oreal"
+                                                      }
+                                  });
+            relationships.Add(
+                              new CompanyBrandRelationship
+                                  {
+                                          OwnerNames = new List<string>() { "Nestle, Nestlé" },
+                                          BrandNames = new List<string>() { "Kitkat" }
+                                  });
+            relationships.Add(
+                              new CompanyBrandRelationship
+                                  {
+                                          OwnerNames = new List<string>() { "Nestle, Nestlé" },
+                                          BrandNames = new List<string>() { "Nespresso" }
+                                  });
+
+            relationships.Add(
+                              new CompanyBrandRelationship
+                                  {
+                                          OwnerNames = new List<string>() { "Cadbury, Cadburies" },
+                                          BrandNames = new List<string>()
+                                                           {
+                                                                   "Dairy Milk",
+                                                                   //"Creme Egg",
+                                                                   //"Roses"
+                                                           }
+                                  });
+
+            relationships.Add(
+                              new CompanyBrandRelationship
+                                  {
+                                          OwnerNames =
+                                                  new List<string>()
+                                                      {
+                                                              "Bayer",
+                                                              "Bayer AG",
+                                                              "Bayer CropScience",
+                                                              "Bayer BioScience",
+                                                              "Bayer Pharma",
+                                                              "Bayer Consumer Care",
+                                                              "Bayer Animal Health",
+                                                      },
+                                          BrandNames =
+                                                  new List<string>()
+                                                      {
+                                                              //"Miles Laboratories",
+                                                              //"Miles Canada",
+                                                              //"Cutter Laboratories",
+                                                              //"Alka-Seltzer",
+                                                              //"Flintstones vitamins",
+                                                              //"One-A-Day vitamins",
+                                                              //"Cutter insect repellent",
+                                                              //"Bomac Group",
+                                                              //"Xofigo",
+                                                              //"Aventis",
+                                                              //"Sanofi",
+                                                              "LibertyLink",
+                                                              //"Jatropha",
+                                                              //"Yasmin",
+                                                              //"Nexavar",
+                                                              //"Kogenate"
+                                                      }
+                                  });
+
+            return relationships;
 
             // https://www.googleapis.com/freebase/v1/search?query=bob&key=<YOUR_API_KEY>
 
@@ -164,40 +288,6 @@
         private static IEnumerable<string> GetPages(string path)
         {
             return Directory.GetFiles(path, "*.htm*", SearchOption.AllDirectories);
-        }
-
-        private static void Main(string[] args)
-        {
-            // When scraping:
-
-            //var doc = new HtmlDocument();
-            //using (var client = new WebClient())
-            //{
-            //    var filename = Path.GetTempFileName();
-            //    client.DownloadFile("http://en.wikipedia.org/wiki/Nestl%C3%A9", filename);
-            //    doc.Load(filename);
-            // Move rest of the method in here
-            //}
-
-            // When using local files: 
-
-            var knownCompanies = GetKnownCompanies();
-
-            var pages = GetPages("C:/Users/Alice/Desktop/TestDocuments");
-
-            var candidates = GetCandidatesFromPages(pages, knownCompanies);
-
-            var file = new System.IO.StreamWriter("C:/Users/Alice/Desktop/Candidates.txt");
-
-            foreach (var candidate in candidates)
-            {
-                file.WriteLine("Page title: " + candidate.PageTitle + "\r Known company: " + candidate.KnownCompany + "\r Html & Text: " + candidate.CandidateHtmlAndText + "\r ");
-                Console.WriteLine(candidate.PageTitle + ' ' + candidate.KnownCompany + ' ' + candidate.CandidateHtmlAndText);
-            }
-
-            file.Close();
-
-            Console.ReadLine();
         }
     }
 }
