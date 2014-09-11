fa64dc2e0d66f338bfd421327ef0155751412639
WheelySnail <alice.waddicor@gmail.com>
2014-09-11 15:07:06 +0100

    Fix issues where only companies with both products AND brands are returned, remove any brands which are returned as null

--- CandidateParsingAgilityPack/FreeBaseHelpers.cs
+++ CandidateParsingAgilityPack/FreeBaseHelpers.cs
@@ -2,6 +2,8 @@ namespace CandidateParsingAgilityPack
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
     using System.Net.Http;
     using System.Net.Http.Headers;
 
@@ -9,9 +11,11 @@ namespace CandidateParsingAgilityPack
 
     using Newtonsoft.Json;
 
+    using sun.net.www.content.image;
+
     internal class FreeBaseHelpers
     {
-        private const string API_KEY = "";
+        private const string API_KEY = "AIzaSyAnlfYJbox67a_jRXUv_9SbGHcfvG0ldbU";
 
         private const String url = "https://www.googleapis.com/freebase/v1/mqlread";
 
@@ -39,26 +43,85 @@ namespace CandidateParsingAgilityPack
 
         internal static List<CompanyAndBrands> GetKnownCompanyBrandRelationshipsFromConsumerCompanies()
         {
+            var brandsResponse = new FreeBaseConsumerCompanyResponse();
+            var productsResponse = new FreeBaseConsumerCompanyResponse();
+
             // TODO this query is returning company brand relationships where the company has BOTH products and brands - only 241 of 2119 possible relations. Query both and combine
-            string companiesQuery =
-                    "?query=[{\"type\":\"/business/consumer_company\",\"id\": null,\"name\": null,\"brands\":[{\"brand\": null}],\"products\":[{\"consumer_product\": null}],\"limit\":2119}]&key="
+            string companiesWithBrandsQuery =
+                    "?query=[{\"type\":\"/business/consumer_company\",\"id\": null,\"name\": null,\"brands\":[{\"brand\": null}],\"limit\":20}]&key="
+                    + API_KEY;
+
+            string companiesWithProductsQuery =
+                    "?query=[{\"type\":\"/business/consumer_company\",\"id\": null,\"name\": null,\"products\":[{\"consumer_product\": null}],\"limit\":20}]&key="
                     + API_KEY;
 
             var client = new HttpClient();
             client.BaseAddress = new Uri(url);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            HttpResponseMessage reponse = client.GetAsync(companiesQuery).Result;
 
-            if (reponse.IsSuccessStatusCode)
+            HttpResponseMessage BrandsResponseMessage = client.GetAsync(companiesWithBrandsQuery).Result;
+
+            if (BrandsResponseMessage.IsSuccessStatusCode)
             {
-                var responseString = reponse.Content.ReadAsStringAsync().Result;
-                var brands = JsonConvert.DeserializeObject<FreeBaseConsumerCompanyResponse>(responseString);
-                return MapFreeBaseConsumerCompaniesToCompanyBrandRelationship(brands.Companies);
+                var responseString = BrandsResponseMessage.Content.ReadAsStringAsync().Result;
+                brandsResponse = JsonConvert.DeserializeObject<FreeBaseConsumerCompanyResponse>(responseString);
             }
             else
             {
                 throw new Exception();
             }
+
+            HttpResponseMessage productsReponseMessage = client.GetAsync(companiesWithProductsQuery).Result;
+
+            if (productsReponseMessage.IsSuccessStatusCode)
+            {
+                var responseString = productsReponseMessage.Content.ReadAsStringAsync().Result;
+                productsResponse = JsonConvert.DeserializeObject<FreeBaseConsumerCompanyResponse>(responseString);
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+            var deDupedCompanyAndBrandsList = new List<CompanyAndBrands>();
+
+            var companiesAndBrands = MapFreeBaseConsumerCompaniesToCompanyBrandRelationship(brandsResponse.Companies); 
+            var companiesAndProducts = MapFreeBaseConsumerCompaniesToCompanyBrandRelationship(productsResponse.Companies); 
+
+            // Add product names to companies already present
+            foreach (var companyAndBrands in companiesAndBrands)
+            {
+                deDupedCompanyAndBrandsList.Add(companyAndBrands);
+
+                foreach (var companyAndProducts in companiesAndProducts)
+                {
+                    if (companyAndBrands.CompanyNames.FirstOrDefault() == companyAndProducts.CompanyNames.FirstOrDefault())
+                    {
+                        companyAndBrands.BrandNames.AddRange(companyAndProducts.BrandNames);
+                    }
+                }
+            }
+
+            foreach (var companyAndProduct in companiesAndProducts)
+            {
+                if (
+                        !deDupedCompanyAndBrandsList.Exists(
+                                                           cb =>
+                                                           cb.CompanyNames.FirstOrDefault()
+                                                           == companyAndProduct.CompanyNames.FirstOrDefault()))
+                {
+                    deDupedCompanyAndBrandsList.Add(companyAndProduct);
+                }
+            }
+
+            // Remove any brands which were returned from FreeBase as 'null'
+
+            foreach (var companyAndBrands in deDupedCompanyAndBrandsList)
+            {
+                companyAndBrands.BrandNames.RemoveAll(bn => bn == null);
+            }
+
+            return deDupedCompanyAndBrandsList;
         }
 
         private static List<CompanyAndBrands> MapFreeBaseConsumerCompaniesToCompanyBrandRelationship(
@@ -70,14 +133,20 @@ namespace CandidateParsingAgilityPack
                 if (freebaseConsumerCompany != null)
                 {
                     var allBrandsAndProducts = new List<string>();
-                    foreach (var brand in freebaseConsumerCompany.Brands)
+                    if (freebaseConsumerCompany.Brands != null)
                     {
-                        allBrandsAndProducts.Add(brand.Brand);
+                        foreach (var brand in freebaseConsumerCompany.Brands)
+                        {
+                            allBrandsAndProducts.Add(brand.Brand);
+                        }
                     }
 
-                    foreach (var product in freebaseConsumerCompany.Products)
+                    if (freebaseConsumerCompany.Products != null)
                     {
-                        allBrandsAndProducts.Add(product.Product);
+                        foreach (var product in freebaseConsumerCompany.Products)
+                        {
+                            allBrandsAndProducts.Add(product.Product);
+                        }
                     }
 
                     companyBrandRelationships.Add(
